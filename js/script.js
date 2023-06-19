b4ab1146af63b5a7b3f22e091386ebce91b92a32
thekidisalright <dev.alexandresilva@gmail.com>
2023-06-19 14:26:45 -0300

    hamburger menu works

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
     const closeIcon = document.querySelector('.navbar-close-icon');
+    const burgerIcon = document.querySelector('.navbar-burger-icon');
+    const body = document.querySelector('#body');
     function removeClass() {
-        const body = document.querySelector('#body');
         body.classList.remove('mobile-menu');
+        closeIcon.style.display = 'none';
+        burgerIcon.style.display = 'block';
     }; 
+    function addClass() {
+        body.classList.add('mobile-menu');
+        closeIcon.style.display = 'block';
+        burgerIcon.style.display = 'none';
+    };
     closeIcon.addEventListener('click', removeClass);
+    burgerIcon.addEventListener('click', addClass);
+
 });
