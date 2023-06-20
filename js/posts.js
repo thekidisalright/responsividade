340874b04b86da057fdd752e8290a4f5bf64a9e6
thekidisalright <dev.alexandresilva@gmail.com>
2023-06-20 12:48:16 -0300

    creating array for posts

---
+++ js/posts.js
@@ -0,0 +1,15 @@
+
+    const post = {
+    pagetitle: 'Blade Runner - Booklire',
+
+    titulo:'Blade Runner - Philip K. Dick', 
+
+    author:' Alexandre Silva',
+
+    date:'5 de maio de 2023',
+
+    content:'<p>Androides Sonham com Ovelhas Elétricas?, ou Blade Runner, é um romance de ficção científica escrito por Philip K. Dick e publicado em 1968. A história se passa em um futuro pós-apocalíptico, onde a Terra está em ruínas após uma guerra nuclear, e grande parte da população emigrou para outros planetas. Aqueles que permaneceram na Terra vivem em um ambiente decadente e desolado.</p><p>O livro acompanha o protagonista Rick Deckard, um caçador de recompensas que tem como missão aposentar androides fugitivos, conhecidos como replicantes. Nessa sociedade distópica, os androides são criados para serem praticamente indistinguíveis dos seres humanos, exceto por sua falta de empatia e capacidade emocional. Deckard enfrenta dilemas éticos ao questionar a linha tênue que separa a humanidade dos replicantes.</p><p>Uma das questões centrais do livro é a reflexão sobre a natureza da identidade e da humanidade. Dick explora a ideia de que a empatia, a capacidade de sentir e compartilhar as emoções dos outros, é o que realmente define a humanidade. Os replicantes, embora sejam feitos de matéria orgânica, carecem dessa qualidade essencial.</p><p>Além disso, a obra aborda temas como a solidão, a busca por significado e a natureza da realidade. O protagonista, Deckard, vive em uma sociedade distópica onde a posse de animais reais é um sinal de status, enquanto animais artificiais são usados como substitutos. Ele próprio possui uma ovelha elétrica, mas almeja ter uma ovelha real para restaurar sua conexão com a natureza e com a autenticidade.</p><p>"Androides Sonham com Ovelhas Elétricas?" é conhecido por sua narrativa complexa, cheia de camadas de realidade e questionamentos filosóficos. A obra foi a inspiração principal para o filme "Blade Runner", dirigido por Ridley Scott, que ampliou ainda mais o impacto e a influência do livro.</p><p>Em suma, o livro de Philip K. Dick é uma obra-prima da ficção científica que mergulha em questões profundas sobre o que significa ser humano. Com sua prosa cativante e seu olhar crítico para a sociedade e a natureza da existência, "Androides Sonham com Ovelhas Elétricas?" continua a desafiar e fascinar os leitores até os dias de hoje.</p>',
+};
+
+const mudartitulo = document.querySelector('.bladetitle');
+mudartitulo.innerText = post.titulo;
--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const closeIcon = document.querySelector('.navbar-close-icon');
-    const burgerIcon = document.querySelector('.navbar-burger-icon');
-    const body = document.querySelector('#body');
-    function removeClass() {
-        body.classList.remove('mobile-menu');
-        closeIcon.style.display = 'none';
-        burgerIcon.style.display = 'block';
-    }; 
-    function addClass() {
-        body.classList.add('mobile-menu');
-        closeIcon.style.display = 'block';
-        burgerIcon.style.display = 'none';
-    };
-    closeIcon.addEventListener('click', removeClass);
-    burgerIcon.addEventListener('click', addClass);
+document.addEventListener('DOMContentLoaded', function(){
+    const closeIcon = document.querySelector('.navbar-close');
+        const burgerIcon = document.querySelector('.navbar-burger-icon');
+        body = document.body;
+        function removeClass() {
+            body.removeAttribute('class', 'mobile-menu');
+            closeIcon.style.display = 'none';
+        }; 
+        function addClass() {
+            body.classList.add('mobile-menu');
+            closeIcon.style.display = 'flex';
+        };
+        document.body.onresize = function() {
+            if(body.clientWidth>1024){
+                removeClass();
+            }
+            else{
+                closeIcon.style.display = 'flex';
+            }
+        };
+        closeIcon.addEventListener('click', removeClass);
+        burgerIcon.addEventListener('click', addClass);
 
+
+    
 });
+    
+
+    
+
