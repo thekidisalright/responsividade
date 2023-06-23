9e76d55a8ec84c5c062c76ba41b62d787c3e798f
Alexandre Silva <dev.alexandresilva@gmail.com>
2023-06-23 06:50:33 -0300

    new html and minor alterations in js

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,6 @@
 const posts = [
     {
-        numero: '0',
+        numero: 0,
         
         pagetitle: 'Carrie, a estranha - Booklire',
 
@@ -21,7 +21,7 @@ const posts = [
         artigo: '<p>"Carrie" é um romance de terror escrito por Stephen King, que narra a vida de uma adolescente chamada Carrie White. Carrie é uma jovem solitária e marginalizada, constantemente vítima de bullying e abusos tanto na escola quanto em casa. Sua mãe, uma mulher religiosa e extremista, impõe uma educação rígida e opressiva sobre ela. No entanto, Carrie descobre que possui poderes telecinéticos, que se manifestam de maneira imprevisível e intensificam-se com suas emoções.</p><p>À medida que a história se desenrola, o leitor testemunha o crescente sofrimento de Carrie e sua luta para se encaixar em um mundo hostil. Eventualmente, uma brincadeira cruel no baile de formatura desencadeia a fúria e a vingança de Carrie. Utilizando seus poderes sobrenaturais, ela causa uma série de eventos catastróficos, resultando em caos e morte.</p><p>"Carrie" é um livro que explora temas profundos, como a crueldade humana, o isolamento, o abuso e a marginalização. O autor retrata habilmente a transformação de uma jovem indefesa em uma força avassaladora, alimentada pelo desejo de vingança. A narrativa é pontuada por elementos de horror e suspense, culminando em um clímax sangrento e arrepiante.</p><p>Publicado em 1974, "Carrie" foi o primeiro romance publicado por Stephen King e consolidou seu lugar como um dos principais escritores de terror da época. A obra foi adaptada para o cinema em 1976, tornando-se um sucesso e impulsionando ainda mais a carreira do autor. Com seu enredo cativante e personagens memoráveis, "Carrie" continua sendo um marco do gênero de terror até os dias de hoje.</p>'
     },
     {
-        numero: '1',
+        numero: 1,
         
         pagetitle: 'O Ladrão de Raios - Booklire',
 
@@ -44,7 +44,7 @@ const posts = [
         artigo:'<p>"Percy Jackson e o Ladrão de Raios", escrito por Rick Riordan, é um livro de fantasia que introduz os leitores ao mundo fascinante dos semideuses. A história gira em torno de Percy Jackson, um garoto de doze anos com dificuldades de aprendizado e uma vida cheia de problemas. No entanto, tudo muda quando ele descobre que é um semideus, filho de Poseidon, o deus do mar.</p><p>Ao ser enviado para o Acampamento Meio-Sangue, Percy descobre que há um universo oculto de deuses, monstros e mitologia grega ao seu redor. Ele se junta a outros semideuses e embarca em uma missão perigosa para recuperar o raio-mestre de Zeus, que foi roubado.</p><p>A narrativa de Riordan é envolvente, repleta de ação e reviravoltas emocionantes. Percy e seus amigos enfrentam desafios perigosos e criaturas mitológicas enquanto lutam contra o tempo para evitar uma guerra entre os deuses do Olimpo. Ao longo da jornada, Percy descobre mais sobre seu passado e seu destino como um dos heróis profetizados.</p><p>Além da emocionante trama, o livro aborda temas importantes como amizade, coragem e aceitação pessoal. Percy é um protagonista carismático e realista, com suas próprias inseguranças e problemas, o que o torna facilmente identificável para os leitores. A narrativa também explora a rica mitologia grega de uma forma acessível e divertida, despertando o interesse dos leitores pela história antiga.</p><p>"Percy Jackson e o Ladrão de Raios" é o primeiro livro de uma série que cativou milhões de fãs em todo o mundo. Com sua combinação de aventura, humor e mitologia, a história de Percy Jackson continua a encantar leitores de todas as idades, deixando-os ansiosos para mergulhar nas próximas aventuras do semideus e seus amigos no mundo dos deuses.</p>'
     },
     {
-        numero: '2',
+        numero: 2,
         
         pagetitle: 'It, a Coisa - Booklire',
 
@@ -67,7 +67,7 @@ const posts = [
         artigo:'<p>"It: A Coisa", escrito por Stephen King, é um romance de terror que se tornou um clássico do gênero. A história se passa na cidade fictícia de Derry, no Maine, onde uma entidade maligna conhecida como Pennywise, o palhaço dançarino, aterroriza a população, especialmente crianças, a cada 27 anos.</p><p>O livro alterna entre duas linhas temporais: a infância dos personagens na década de 1950, quando enfrentaram Pennywise pela primeira vez, e a vida adulta deles, quando são chamados de volta a Derry para enfrentar novamente o mal que acreditavam ter derrotado.</p><p>King apresenta um elenco de personagens memoráveis, o "Clube dos Otários", um grupo de crianças marginalizadas que se unem para enfrentar seus medos e confrontar Pennywise. Cada personagem tem sua própria luta pessoal e enfrenta traumas e adversidades em suas vidas, tornando-os mais complexos e reais.</p><p>Além de ser uma história de terror, "It: A Coisa" também é uma narrativa sobre amizade, crescimento e superação. Os laços criados entre as crianças, baseados na confiança e no apoio mútuo, desempenham um papel fundamental na luta contra o mal que assola Derry.</p><p>Uma das características marcantes do livro é a habilidade de Stephen King em criar cenas assustadoras e atmosferas opressivas. Pennywise, com sua aparência grotesca e capacidade de se transformar nos medos mais profundos de suas vítimas, se tornou um dos vilões mais icônicos da literatura de horror.</p><p>Além disso, King explora temas mais profundos, como a infância perdida, a influência do medo em nossas vidas e a natureza do mal. "It: A Coisa" é uma obra que vai além dos sustos superficiais, mergulhando nas profundezas da psicologia humana e nas batalhas internas que cada personagem enfrenta.</p><p>Em resumo, "It: A Coisa" é um livro envolvente e assustador que captura a imaginação dos leitores. Com sua combinação de terror, amizade e temas mais profundos, a obra se tornou um marco na literatura de Stephen King e continua a cativar os leitores com sua narrativa arrepiante e personagens inesquecíveis.</p>'
     },
     {
-        numero: '3',
+        numero: 3,
         
         pagetitle: 'Fogo e Sangue - Booklire',
 
@@ -90,7 +90,7 @@ const posts = [
         artigo:'<p>"Fogo e Sangue" é um livro escrito por George R. R. Martin que se passa no universo de "As Crônicas de Gelo e Fogo", mas funciona como uma obra independente. O livro é uma narrativa histórica detalhada que mergulha na história da dinastia Targaryen, a família governante de Westeros antes dos eventos da série principal.</p><p>A obra é dividida em duas partes: "A Ascensão dos Dragões" e "A Dança dos Dragões". A primeira parte abrange desde a chegada dos Targaryen a Westeros até a guerra civil conhecida como a Dança dos Dragões, enquanto a segunda parte se concentra nas consequências desse conflito.</p><p>"Fogo e Sangue" mergulha nos bastidores do governo Targaryen, explorando as intrigas políticas, os casamentos arranjados, as guerras e os dragões que moldaram a história do continente. George R. R. Martin apresenta aos leitores uma infinidade de personagens fascinantes, desde reis e rainhas até cavaleiros e conselheiros astutos.</p><p>O livro é famoso por sua riqueza de detalhes e estilo de escrita que se assemelha a uma crônica histórica. Martin mergulha nas complexidades da política e da dinâmica de poder, oferecendo aos leitores uma visão mais ampla do mundo de Westeros e dos eventos que levaram ao início da série principal.</p><p>Além disso, "Fogo e Sangue" expande o conhecimento sobre os dragões, criaturas majestosas e poderosas que desempenharam um papel crucial na história dos Targaryen. Os dragões são retratados como seres temíveis e imponentes, capazes de influenciar o destino dos reinos.</p><p>Os fãs de "As Crônicas de Gelo e Fogo" encontrarão em "Fogo e Sangue" um mergulho profundo na história e no passado de Westeros. A obra oferece um panorama fascinante e detalhado da dinastia Targaryen, revelando segredos e eventos que moldaram o continente e lançando luz sobre alguns dos mistérios apresentados na série principal.</p><p>Em resumo, "Fogo e Sangue" é uma leitura obrigatória para os fãs de "As Crônicas de Gelo e Fogo" que desejam se aprofundar na história e nas origens da dinastia Targaryen. Com sua narrativa rica em detalhes, personagens cativantes e intriga política, o livro oferece uma visão fascinante do passado de Westeros e enriquece ainda mais o vasto universo criado por George R. R. Martin.</p>'
     },
     {
-        numero: '4',
+        numero: 4,
         
         pagetitle: 'A Guerra Dos Tronos - Booklire',
 
@@ -113,7 +113,7 @@ const posts = [
         artigo:'<p>"A Guerra dos Tronos", escrito por George R. R. Martin, é o primeiro livro da aclamada série "As Crônicas de Gelo e Fogo". Ambientado em um mundo medieval fictício, o livro apresenta um enredo intrincado repleto de política, intriga, traição e batalhas épicas pelo controle do Trono de Ferro.</p><p>A história se desenrola em Westeros, um continente dividido em sete reinos, cada um governado por uma casa nobre. Quando o rei Robert Baratheon chama seu amigo de infância, Eddard Stark, para se tornar sua Mão do Rei, uma série de eventos é desencadeada, levando a um jogo mortal de poder entre várias casas nobres.</p><p>"A Guerra dos Tronos" é famoso por sua narrativa multi-perspectiva, onde os capítulos alternam entre diferentes personagens, permitindo que o leitor experimente os eventos pelos olhos de diversos protagonistas. Isso oferece uma visão ampla das intrigas palacianas, alianças instáveis e tramas políticas que se desenrolam em todo o reino.</p><p>Além disso, o livro é conhecido por seus personagens complexos e bem desenvolvidos. Desde os nobres e ambiciosos Lannisters até os membros exilados da Casa Targaryen, cada personagem é único e cativante, repletos de virtudes e falhas. A riqueza psicológica dos personagens é um dos pontos fortes da obra, pois eles enfrentam escolhas difíceis e lidam com as consequências de suas ações.</p><p>Outro aspecto marcante da obra é a violência e a brutalidade retratadas nas batalhas e nos confrontos entre as casas. Martin não hesita em mostrar as consequências terríveis da guerra, desafiando a visão idealizada dos conflitos medievais.</p><p>"A Guerra dos Tronos" também é conhecido por suas reviravoltas surpreendentes e inesperadas, que mantêm os leitores ávidos por mais. Ninguém está seguro nesse mundo implacável, onde personagens importantes podem ser mortos de forma repentina e chocante.</p><p>Em resumo, "A Guerra dos Tronos" é uma obra-prima da fantasia épica que apresenta um mundo rico e complexo, repleto de personagens memoráveis, intriga política e batalhas sangrentas. Com sua narrativa envolvente e imprevisível, o livro cativa os leitores e estabelece as bases para uma das séries mais populares e influentes da literatura moderna.</p>'
     },
     {
-        numero: '5',
+        numero: 5,
 
         pagetitle: 'Blade Runner - Booklire',
 
@@ -137,27 +137,24 @@ const posts = [
     }
 ];
 
-var idpagina;
 
-if(document.getElementsByClassName == 5)
 
-function changeindex(){
-    const pagetitle = document.querySelector('title');
+// function changeindex(){
+//     const pagetitle = document.querySelector('title');
 
-    pagetitle = 'Home - Booklire';
-}
+//     pagetitle = 'Home - Booklire';
+// }
 
-function changearticle(x){
-    const pagetitle = document.querySelector('title');
-    const titulo = document.querySelector('.titulo');
-    const author = document.querySelector('.author');
-    const date = document.querySelector('.date');
-    const artigo = document.querySelector('.artigo');
+const pagetitle = document.querySelector('title');
+const titulo = document.querySelector('.titulo');
+const author = document.querySelector('.author');
+const date = document.querySelector('.date');
+const artigo = document.querySelector('.artigo');
 
-    pagetitle.textContent = posts[x].pagetitle;
-    titulo.textContent = posts[x].titulo;
-    author.insertAdjacentHTML('beforeend', posts[x].author);
-    date.textContent = posts[x].date;
-    date.setAttribute('datetime', posts[x].datetime);
-    artigo.innerHTML = posts[x].artigo;
-};
+    // pagetitle.textContent = posts[x].pagetitle;
+    // titulo.textContent = posts[x].titulo;
+    // author.insertAdjacentHTML('beforeend', posts[x].author);
+    // date.textContent = posts[x].date;
+    // date.setAttribute('datetime', posts[x].datetime);
+    // artigo.innerHTML = posts[x].artigo;
+
